11ec8b5072edc6758d23c212c7731d2326ecf1e6
asthasilicon <mohantaastha2@gmail.com>
2024-02-04 02:09:09 +0530

    mongodb-operation-updaed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 REPO_NAME = "mongodbconnectpkg"
 PKG_NAME= "mongodb_connection"
 AUTHOR_USER_NAME = "asthalochan"
